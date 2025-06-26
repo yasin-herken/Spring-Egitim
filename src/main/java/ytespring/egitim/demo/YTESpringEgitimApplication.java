8383ffa4afc26b8a4b691dd7f8d6d08eb1f116fc
Yasin HERKEN <yasin.herken@tubitak.gov.tr>
2025-06-26 21:40:29 +0300

    Main branch oluşturuldu

---
+++ src/main/java/ytespring/egitim/demo/YTESpringEgitimApplication.java
@@ -0,0 +1,12 @@
+package ytespring.egitim.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YTESpringEgitimApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YTESpringEgitimApplication.class, args);
+    }
+}
